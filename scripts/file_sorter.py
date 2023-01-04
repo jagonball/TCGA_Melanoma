ec5a6b8c6bcbefc6ee63867680d1e5c7db00652e
Johnathan Lin <jagonball@gmail.com>
2023-01-04 22:59:04 +0800

    update and add step - Added function create_folder. - Added stage part.

--- scripts/file_sorter.py
+++ scripts/file_sorter.py
@@ -6,49 +6,90 @@ at https://portal.gdc.cancer.gov/projects/TCGA-SKCM
 import os
 import subprocess
 import pandas as pd
+import numpy as np
 import shutil
 import json
 
 def main():
     ### input parameters ###
-    main_data = './data/nationwidechildrens.org_clinical_patient_skcm.txt'
-    json_files = './data/files.2022-12-27.json'
-    json_cases = './data/cases.2022-12-27.json'
-    download_folder = './data/downloads/'
+    project_name = 'TCGA_SKCM'
+    output_folder = 'C:/Repositories/Melanoma_TCGA/analysis'
+    main_data = 'C:/Repositories/Melanoma_TCGA/data/clinical_patient_skcm.txt'
+    json_files = 'C:/Repositories/Melanoma_TCGA/data/files.2022-12-27.json'
+    json_cases = 'C:/Repositories/Melanoma_TCGA/data/cases.2022-12-27.json'
+    download_folder = 'C:/Repositories/Melanoma_TCGA/data/downloads/'
     download_compressed = 'gdc_download_20221227_162931.686561.tar.gz'
     
 
+    ### Create project folder if not already exist
+    create_folder(project_name, output_folder)
+
     ### Unzip downloaded file ###
     # Check if 'extracted' folder exist, if not, create one.
-    if not os.path.exists(f'{download_folder}extracted/'):
-        print(f'Folder: {download_folder}extracted/ not found, creating...')
-        os.mkdir(f'{download_folder}extracted/')
-    # Unzip to 'download_folder/extracted'
-    subprocess.run(f'tar -xf {download_folder}{download_compressed}\
-                         -C {download_folder}extracted/')
-    # Read the 'MANIFEST.txt' file for file information
-    file_info = pd.read_table(f'{download_folder}extracted/MANIFEST.txt',
-                              low_memory=False)
-    #print(file_info)
-    # Filter for state 'validated', to ignore the 'annotations.txt'
-    file_info = file_info[file_info['state'] == 'validated']
-    #print(file_info)
-    ## Move the files into 'temp_folder'
-    # Check if 'temp_folder' folder exist, if not, create one.
-    if not os.path.exists(f'{download_folder}temp_folder/'):
-        print(f'Folder: {download_folder}temp_folder/ not found, creating...')
-        os.mkdir(f'{download_folder}temp_folder/')
-    for file in file_info['filename']:
-        print(file)
-        file_name = file.split('/')[1]
-        print(file_name)
-        shutil.move(f'{download_folder}extracted/{file}',
-                    f'{download_folder}temp_folder/{file_name}')
+#    if not os.path.exists(f'{download_folder}extracted/'):
+#        print(f'Folder: {download_folder}extracted/ not found, creating...')
+#        os.mkdir(f'{download_folder}extracted/')
+#    # Unzip to 'download_folder/extracted'
+#    subprocess.run(f'tar -xf {download_folder}{download_compressed}\
+#                         -C {download_folder}extracted/')
+#    # Read the 'MANIFEST.txt' file for file information
+#    file_info = pd.read_table(f'{download_folder}extracted/MANIFEST.txt',
+#                              low_memory=False)
+#    #print(file_info)
+#    # Filter for state 'validated', to ignore the 'annotations.txt'
+#    file_info = file_info[file_info['state'] == 'validated']
+#    #print(file_info)
+#    ## Move the files into 'temp_folder'
+#    # Check if 'temp_folder' folder exist, if not, create one.
+#    if not os.path.exists(f'{download_folder}temp_folder/'):
+#        print(f'Folder: {download_folder}temp_folder/ not found, creating...')
+#        os.mkdir(f'{download_folder}temp_folder/')
+#    for file in file_info['filename']:
+#        print(file)
+#        file_name = file.split('/')[1]
+#        print(file_name)
+#        shutil.move(f'{download_folder}extracted/{file}',
+#                    f'{download_folder}temp_folder/{file_name}')
+
+    ### Read main_data into DataFrame
+    main_df = pd.read_table(main_data, sep = '\t', header = [0, 1, 2])
+    #print(main_df['ajcc_pathologic_tumor_stage'])
+    ## Select tumor stage column, squeeze into Series,
+    ## then reorder the unique stage values.
+    unique_stages = np.sort(main_df['ajcc_pathologic_tumor_stage']\
+                            .squeeze().unique())
+    print(unique_stages)
+    ## Create a folder for each tumor stage,
+    ## then for each patient create folder with bcr_patient_barcode
+    for stage in unique_stages:
+        # Check if the stage folder exist, if not, create one.
+        if not os.path.exists(f'./data/'):
+            print(f'Folder: {download_folder}extracted/ not found, creating...')
+            os.mkdir(f'{download_folder}extracted/')
+
+
+
+
+    def create_folder(folder_name, output_folder):
+        """Create folder if not already exist.
+
+        :param folder_name: The folder name.
+        :type folder_name: str
+        :param output_folder: The path to check for the folder.
+        :type output_folder: str
+        :return: The full path to inside the folder.
+        :rtype: str
+        """
+        if folder_name not in os.listdir(output_folder):
+            os.mkdir(os.path.join(output_folder, folder_name))
+        final_folder = os.path.join(output_folder, folder_name)
+        return final_folder
+
 
 '''
 To do list:
 1. Create folders based on tumor stage information
-from file './data/nationwidechildrens.org_clinical_patient_skcm.txt'
+from file './data/clinical_patient_skcm.txt'
     [Not Available]
     I/II NOS
     Stage 0
