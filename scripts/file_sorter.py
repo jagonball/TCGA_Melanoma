dd98ac07f3d7a2be3cfbd4c445313320aba0068a
Johnathan Lin <jagonball@gmail.com>
2023-01-05 20:24:17 +0800

    Modify structure - Added Path from pathlib. - Check folder name before creating one. - Next will be moving file and rename.

--- scripts/file_sorter.py
+++ scripts/file_sorter.py
@@ -5,6 +5,8 @@ at https://portal.gdc.cancer.gov/projects/TCGA-SKCM
 '''
 import os
 import subprocess
+import re
+from pathlib import Path
 import pandas as pd
 import numpy as np
 import shutil
@@ -13,43 +15,48 @@ import json
 def main():
     ### input parameters ###
     project_name = 'TCGA_SKCM'
-    output_folder = 'C:/Repositories/Melanoma_TCGA/analysis'
-    main_data = 'C:/Repositories/Melanoma_TCGA/data/clinical_patient_skcm.txt'
-    json_files = 'C:/Repositories/Melanoma_TCGA/data/files.2022-12-27.json'
-    json_cases = 'C:/Repositories/Melanoma_TCGA/data/cases.2022-12-27.json'
-    download_folder = 'C:/Repositories/Melanoma_TCGA/data/downloads/'
-    download_compressed = 'gdc_download_20221227_162931.686561.tar.gz'
-    
+    output_folder = Path('C:/Repositories/Melanoma_TCGA/analysis/')
+    main_data = Path('C:/Repositories/Melanoma_TCGA/data/clinical_patient_skcm.txt')
+    json_files = Path('C:/Repositories/Melanoma_TCGA/data/files.2022-12-27.json')
+    json_cases = Path('C:/Repositories/Melanoma_TCGA/data/cases.2022-12-27.json')
+    download_folder = Path('C:/Repositories/Melanoma_TCGA/data/0104/')
+    download_compressed = 'gdc_download_20230104_090654.618910.tar.gz'
+
 
     ### Create project folder if not already exist
-    create_folder(project_name, output_folder)
+    project_name = replace_special_chars(project_name)
+    project_folder = create_folder(project_name, output_folder)
+
 
     ### Unzip downloaded file ###
-    # Check if 'extracted' folder exist, if not, create one.
-#    if not os.path.exists(f'{download_folder}extracted/'):
-#        print(f'Folder: {download_folder}extracted/ not found, creating...')
-#        os.mkdir(f'{download_folder}extracted/')
-#    # Unzip to 'download_folder/extracted'
-#    subprocess.run(f'tar -xf {download_folder}{download_compressed}\
-#                         -C {download_folder}extracted/')
-#    # Read the 'MANIFEST.txt' file for file information
-#    file_info = pd.read_table(f'{download_folder}extracted/MANIFEST.txt',
-#                              low_memory=False)
-#    #print(file_info)
-#    # Filter for state 'validated', to ignore the 'annotations.txt'
-#    file_info = file_info[file_info['state'] == 'validated']
-#    #print(file_info)
-#    ## Move the files into 'temp_folder'
-#    # Check if 'temp_folder' folder exist, if not, create one.
-#    if not os.path.exists(f'{download_folder}temp_folder/'):
-#        print(f'Folder: {download_folder}temp_folder/ not found, creating...')
-#        os.mkdir(f'{download_folder}temp_folder/')
-#    for file in file_info['filename']:
-#        print(file)
-#        file_name = file.split('/')[1]
-#        print(file_name)
-#        shutil.move(f'{download_folder}extracted/{file}',
-#                    f'{download_folder}temp_folder/{file_name}')
+    # Create 'extracted' folder if not already exist.
+    extracted_folder = create_folder('extracted', download_folder)
+    #if not os.path.exists(f'{download_folder}extracted/'):
+    #    print(f'Folder: {download_folder}extracted/ not found, creating...')
+    #    os.mkdir(f'{download_folder}extracted/')
+    # Unzip compressed file to extracted_folder
+    compressed_file = download_folder / download_compressed
+    subprocess.run(['tar', '-xf', compressed_file, '-C', extracted_folder])
+    # Read the 'MANIFEST.txt' file for file information
+    file_info = pd.read_table(extracted_folder/'MANIFEST.txt',
+                              low_memory=False)
+    #print(file_info)
+    # Filter for state 'validated', to ignore the 'annotations.txt'
+    file_info = file_info[file_info['state'] == 'validated']
+    #print(file_info)
+    ## Move the files into 'temp_folder'
+    # Create 'temp_folder' folder if not already exist.
+    temp_folder = create_folder('temp_folder', download_folder)
+    #if not os.path.exists(f'{download_folder}temp_folder/'):
+    #    print(f'Folder: {download_folder}temp_folder/ not found, creating...')
+    #    os.mkdir(f'{download_folder}temp_folder/')
+    for file in file_info['filename']:
+        print(file)
+        file_name = file.split('/')[-1]
+        print(file_name)
+        shutil.move(extracted_folder/file,
+                    temp_folder/file_name)
+
 
     ### Read main_data into DataFrame
     main_df = pd.read_table(main_data, sep = '\t', header = [0, 1, 2])
@@ -62,29 +69,38 @@ def main():
     ## Create a folder for each tumor stage,
     ## then for each patient create folder with bcr_patient_barcode
     for stage in unique_stages:
+        print(stage)
         # Check if the stage folder exist, if not, create one.
-        if not os.path.exists(f'./data/'):
-            print(f'Folder: {download_folder}extracted/ not found, creating...')
-            os.mkdir(f'{download_folder}extracted/')
+        stage = replace_special_chars(stage)
+        print(stage)
+        stage_folder = create_folder(stage, project_folder)
+        # Subset main DataFrame with specific stage
+        #stage_df = main_df[main_df['ajcc_pathologic_tumor_stage'] == stage]
 
 
 
 
-    def create_folder(folder_name, output_folder):
-        """Create folder if not already exist.
+def create_folder(folder_name, output_folder):
+    """Create folder if not already exist.
 
-        :param folder_name: The folder name.
-        :type folder_name: str
-        :param output_folder: The path to check for the folder.
-        :type output_folder: str
-        :return: The full path to inside the folder.
-        :rtype: str
-        """
-        if folder_name not in os.listdir(output_folder):
-            os.mkdir(os.path.join(output_folder, folder_name))
-        final_folder = os.path.join(output_folder, folder_name)
-        return final_folder
+    :param folder_name: The folder name.
+    :type folder_name: str
+    :param output_folder: The path to check for the folder.
+    :type output_folder: str
+    :return: The full path to inside the folder.
+    :rtype: str
+    """
+    final_folder = output_folder / folder_name
+    if folder_name not in os.listdir(output_folder):
+        print(f'Folder "{folder_name}" not found, creating...')
+        os.mkdir(final_folder)    
+    return final_folder
 
+
+def replace_special_chars(str):
+    mod_str = re.sub('[^a-zA-Z0-9 \n\.]', '_', str)
+    mod_str = mod_str.replace(" ", "_")
+    return mod_str
 
 '''
 To do list:
