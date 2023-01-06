a672a4b527fa9dcf5d231542dbb5052c4b5a03c6
Johnathan Lin <jagonball@gmail.com>
2023-01-06 11:29:11 +0800

    Complete first rename - Modify create_folder prompt - rename list of files

--- scripts/file_sorter.py
+++ scripts/file_sorter.py
@@ -7,6 +7,7 @@ import os
 import subprocess
 import re
 from pathlib import Path
+import glob
 import pandas as pd
 import numpy as np
 import shutil
@@ -17,10 +18,10 @@ def main():
     project_name = 'TCGA_SKCM'
     output_folder = Path('C:/Repositories/Melanoma_TCGA/analysis/')
     main_data = Path('C:/Repositories/Melanoma_TCGA/data/clinical_patient_skcm.txt')
-    json_files = Path('C:/Repositories/Melanoma_TCGA/data/files.2022-12-27.json')
-    json_cases = Path('C:/Repositories/Melanoma_TCGA/data/cases.2022-12-27.json')
-    download_folder = Path('C:/Repositories/Melanoma_TCGA/data/0104/')
-    download_compressed = 'gdc_download_20230104_090654.618910.tar.gz'
+    json_files = Path('C:/Repositories/Melanoma_TCGA/data/files.2023-01-06.json')
+    json_cases = Path('C:/Repositories/Melanoma_TCGA/data/cases.2023-01-06.json')
+    download_folder = Path('C:/Repositories/Melanoma_TCGA/data/0106_test/')
+    download_compressed = 'gdc_download_20230106_023217.521073.tar.gz'
 
 
     ### Create project folder if not already exist
@@ -35,6 +36,7 @@ def main():
     #    print(f'Folder: {download_folder}extracted/ not found, creating...')
     #    os.mkdir(f'{download_folder}extracted/')
     # Unzip compressed file to extracted_folder
+    print(f'## Extracting the compressed file to {extracted_folder}...')
     compressed_file = download_folder / download_compressed
     subprocess.run(['tar', '-xf', compressed_file, '-C', extracted_folder])
     # Read the 'MANIFEST.txt' file for file information
@@ -50,10 +52,11 @@ def main():
     #if not os.path.exists(f'{download_folder}temp_folder/'):
     #    print(f'Folder: {download_folder}temp_folder/ not found, creating...')
     #    os.mkdir(f'{download_folder}temp_folder/')
+    print(f'## Moving the extracted files to {temp_folder}...')
     for file in file_info['filename']:
-        print(file)
+        #print(file)
         file_name = file.split('/')[-1]
-        print(file_name)
+        #print(file_name)
         shutil.move(extracted_folder/file,
                     temp_folder/file_name)
 
@@ -65,17 +68,70 @@ def main():
     ## then reorder the unique stage values.
     unique_stages = np.sort(main_df['ajcc_pathologic_tumor_stage']\
                             .squeeze().unique())
-    print(unique_stages)
+    print(f'Unique stages: {unique_stages}')
     ## Create a folder for each tumor stage,
     ## then for each patient create folder with bcr_patient_barcode
     for stage in unique_stages:
-        print(stage)
+        #print(stage)
         # Check if the stage folder exist, if not, create one.
         stage = replace_special_chars(stage)
-        print(stage)
+        #print(stage)
         stage_folder = create_folder(stage, project_folder)
         # Subset main DataFrame with specific stage
         #stage_df = main_df[main_df['ajcc_pathologic_tumor_stage'] == stage]
+
+#    # Read json.
+#    with open(json_files) as file:
+#        files_contents = file.read()
+#    #print(files_contents)
+#    with open(json_cases) as file:
+#        cases_contents = file.read()
+#
+#    # Load json contents into a list of dictionaries.
+#    parsed_json_files = json.loads(files_contents)
+#    #print(parsed_json_files[0:2])#['case_id'])
+#    parsed_json_cases = json.loads(cases_contents)
+#    #print(parsed_json_cases)
+#
+#    files_re_list = ['*.wxs.aliquot_ensemble_masked.maf.gz',
+#                     '*.rna_seq.augmented_star_gene_counts.tsv',
+#                     '*.mirbase21.isoforms.quantification.txt',
+#                     '*.mirbase21.mirnas.quantification.txt']
+#    for name in files_re_list:
+#        target_files_re = str(temp_folder/name)
+#        target_file_list = glob.glob(target_files_re)
+#        print(target_file_list)
+#
+#    for i in target_file_list:
+#        target_file_name = Path(i).name
+#        # Files to keep the name after first ".".
+#        file_id = target_file_name.split('.')[0]
+#        print(file_id)
+#        name_keep = target_file_name.split('.')[1:]
+#        name_keep = '.'.join(name_keep)
+#        print(name_keep)
+#        # Iterate through parsed_json_files for matching file_name.
+#        target_case_id = None
+#        target_submitter_id = None
+#        for i in parsed_json_files:
+#            if i['file_name'] == target_file_name:
+#                #print(i)
+#                if len(i['cases']) != 1:
+#                    print(f'Warning: list "cases" for file '
+#                          f'"{target_file_name}" in "{json_files}" '
+#                          f'are empty or more than one items')
+#                target_case_id = i['cases'][0]['case_id']
+#                #print(target_case_id)
+#                for i in parsed_json_cases:
+#                    if i['case_id'] == target_case_id:
+#                        #print(i)
+#                        target_submitter_id = i['submitter_id']
+#                        #print(target_submitter_id)
+#        # Rename target file if there's match.
+#        if target_submitter_id:
+#            os.rename(temp_folder/target_file_name,
+#                      temp_folder/f'{target_submitter_id}.{name_keep}')
+    
 
 
 
@@ -91,8 +147,9 @@ def create_folder(folder_name, output_folder):
     :rtype: str
     """
     final_folder = output_folder / folder_name
+    print(f'Checking if folder "{folder_name}" exists...')
     if folder_name not in os.listdir(output_folder):
-        print(f'Folder "{folder_name}" not found, creating...')
+        print(f'## Folder "{folder_name}" not found, creating...')
         os.mkdir(final_folder)    
     return final_folder
 
@@ -101,6 +158,9 @@ def replace_special_chars(str):
     mod_str = re.sub('[^a-zA-Z0-9 \n\.]', '_', str)
     mod_str = mod_str.replace(" ", "_")
     return mod_str
+
+
+
 
 '''
 To do list:
@@ -121,16 +181,23 @@ from file './data/clinical_patient_skcm.txt'
     Stage IIIB
     Stage IIIC
     Stage IV
-2. 8 files for each case:
+2. 7 files for each case:
 First 4 rename with 'submitter_id' + originl name after first '.':
+    # Data Type: Masked Somatic Mutation
     '*.wxs.aliquot_ensemble_masked.maf.gz' (unzip then delete .gz)
+    # Data Type: Gene Expression Quantification
     '*.rna_seq.augmented_star_gene_counts.tsv'
+    # Data Type: Isoform Expression Quantification
     '*.mirbase21.isoforms.quantification.txt'
-    '*mirbase21.mirnas.quantification.txt'
+    # Data Type: miRNA Expression Quantification
+    '*.mirbase21.mirnas.quantification.txt'
 Keep these 2:
+    # Data Type: Pathology Report
     '*.80F131FA-7E24-4210-800F-3E6F442CAB6F.PDF'
+    # Data Type: Protein Expression Quantification
     '*TCGA-D3-A3ML-06A-21-A241-20_RPPA_data.tsv'
 Rename with 'submitter_id' + originl name after second '.':
+    # Data Type: Gene Level Copy Number
     '*.gene_level_copy_number.v36.tsv'
 3. Move and rename files in 
 f'{download_folder}temp_folder/' to the above created folders
@@ -140,18 +207,7 @@ rename file with: json_cases 'submitter_id' and json_files 'data_category'
 calculate mean value for each stage folder.
 '''
 
-#    # Read json
-#    with open(json_files) as file:
-#        files_contents = file.read()
-#    #print(files_contents)
-#    with open(json_cases) as file:
-#        cases_contents = file.read()
-#    
-#
-#    # Load json contents into a list of dictionaries
-#    parsed_json_files = json.loads(files_contents)
-#    print(parsed_json_files[0]['case_id'])
-#    parsed_json_cases = json.loads(cases_contents)
+
 
 
 if __name__ == '__main__':
